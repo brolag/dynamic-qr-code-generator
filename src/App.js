fc05c57cd0a4624fb6aa71d96dc83a15c6995bf7
Alfredo Bonilla <info@alfredobonilla.com>
2023-12-01 08:18:37 -0600

    Adjust the QR code to change every hour based on user data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import QRCode from "react-qr-code";
 import sha256 from "crypto-js/sha256";
 import "./App.css";
@@ -7,17 +7,42 @@ function App() {
   const [hash, setHash] = useState("");
   const SECRET = "secret";
 
-  const updateHash = (e) => {
-    const hashDigest = sha256(e.target.value, SECRET);
-    const hash = hashDigest.toString();
-    setHash(hash);
-  }
+  // Dirección de billetera y hash de transacción de NFT ficticios
+  const userWalletAddress = "0x1234567890abcdef1234567890abcdef12345678";
+  const nftTxHash =
+    "0x9abcdef1234567890abcdef1234567890abcdef1234567890abcdef12345678";
+
+  // Función para obtener el valor basado en la fecha y hora actual
+  const getDynamicValue = () => {
+    const now = new Date();
+    const year = now.getUTCFullYear();
+    const month = now.getUTCMonth() + 1; // Ajuste porque getMonth() devuelve 0-11
+    const day = now.getUTCDate();
+    const hour = now.getUTCHours();
+    const second = now.getSeconds();
+
+    return `${year}-${month}-${day}-${hour}-${second}`;
+  };
+
+  useEffect(() => {
+    const updateQRCode = () => {
+      const dynamicValue = getDynamicValue();
+      const combinedValue = `${dynamicValue}-${userWalletAddress}-${nftTxHash}`;
+      const hashDigest = sha256(combinedValue, SECRET);
+      setHash(hashDigest.toString());
+    };
+
+    updateQRCode();
+
+    const interval = setInterval(updateQRCode, 2000); // 2000 ms = 2 segundos
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <div style={{ background: "white", padding: "16px" }}>
-          <input onChange={updateHash} type="text" />
           <QRCode
             size={256}
             style={{ height: "auto", maxWidth: "100%", width: "100%" }}
